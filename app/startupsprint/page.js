76b9461322cfbafba9da7a7c2c51c78614b37595
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-10-08 19:42:07 -0400

    Create page.js

---
+++ app/startupsprint/page.js
@@ -0,0 +1,110 @@
+
+
+function StartupSprint2023() {
+    return (
+      <div>
+          <h1 className="text-4xl font-bold mb-4">
+              Startup Sprint 2023
+          </h1>
+          <h2 className="text-2xl text-primary">
+              Dare to Dream, Take the Leap
+          </h2>
+          <p className="text-lg mb-8">
+            Are you ready to embark on an entrepreneurial journey like no other?
+            Virginia Tech's Entrepreneurship Club, in partnership with the
+            Apex Center for Entrepreneurs, presents {' '}
+            <span className="text-secondary">Startup Sprint</span> – your chance to turn innovative ideas into reality in just one week!
+          </p>
+  
+          <div className="shadow-lg border rounded-lg p-6 mb-10 lg:grid lg:grid-cols-2 ">
+              <div className="lg:w-1/2">
+                  <h2 className="text-xl font-semibold mb-2">📅  Event Dates</h2>
+                  <p className="text-accent">Application Deadline: </p>
+                  <p className="text-accent">Startup Sprint Kickoff: Wednesday November 8th</p>
+                  <p className="text-accent">Submission Deadline: Sunday November 12th</p>
+                  <p className="text-accent">Pitch Day: Wednesday November 15th</p>
+              </div>
+              <div className="lg:w-1/2">
+                  <h2>Apply</h2>
+              </div>
+           </div>
+          <div className="flex flex-wrap -mx-2">
+            <div className="w-full md:w-1/2 px-2 mb-4">
+              <div className="p-6 border shadow-lg rounded-lg">
+                <h2 className="text-2xl font-bold mb-4 text-secondary">
+                💡 What is Startup Sprint?
+                </h2>
+                <p>
+                  The Virginia Tech Startup Sprint is an exhilarating week-long event
+                  where students from all backgrounds come together to build innovative
+                  solutions to real-world problems. Whether you're a coding whiz, a
+                  marketing guru, a design genius, or just bursting with entrepreneurial
+                  spirit, this is your chance to shine!
+                </p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 px-2 mb-4">
+              <div className="p-6 border shadow-lg rounded-lg">
+                <h2 className="text-2xl font-bold mb-4 text-secondary">🌟 Why Should You Join?</h2>
+                <ul className="list-disc">
+                  <li>
+                      <span className="font-semibold text-primary">Hands-On Experience:</span> You're going to be dropped into a fast-paced environment where you'll have to identify a problem, deploy a solution, and craft a business model.
+                  </li>
+                  <li>
+                      <span className="font-semibold text-primary">Teamwork:</span> Form teams with diverse skill sets and learn the art of collaboration.
+                  </li>
+                  <li>
+                      <span className="font-semibold text-primary">Skill Development:</span> Learn to use hyper-modern technologies with workshops led by industry partners.
+                  </li>
+                  <li>
+                      <span className="font-semibold text-primary">Win $$$:</span> Impress our panel of judges, including corporate sponsors like Deloitte and Community Labs.
+                  </li>
+                  <li>
+                      <span className="font-semibold text-primary">Boost Your Resume:</span> Add this exciting experience to your resume.
+                  </li>
+              </ul>
+  
+              </div>
+            </div>
+          </div>
+          <div className="mt-8">
+              <h2 className="font-bold text-2xl text-secondary">Our Corporate Sponsors:</h2>
+            <ul className="list-disc list-inside">
+              <li>
+                <strong>Deloitte:</strong> A global leader in consulting, Deloitte brings expertise in business strategy and innovation to help kickstart your startup journey.
+              </li>
+              <li>
+                <strong>Community Labs:</strong> Leaders in community health solutions, they're here to support your mission to create positive change.
+              </li>
+              <li>
+                <strong>Virginia Tech's Management Department:</strong> Our very own university department joins hands with us to nurture the next generation of entrepreneurial leaders.
+              </li>
+            </ul>
+          </div>
+          <div className="mt-8">
+              <h2 className="font-bold text-2xl text-secondary">Who Can Participate?</h2>
+            <p>
+              This event is open to all Virginia Tech students who are ready to innovate, create, and collaborate. No prior entrepreneurship experience required – just bring your passion and enthusiasm!
+            </p>
+          </div>
+          <div className="mt-8">
+            <p className="text-lg">
+              <strong>Stay tuned for updates, insights, and exclusive behind-the-scenes content on our website and social media channels.</strong>
+            </p>
+            <p>
+              Follow us on [Insert Social Media Links] to stay in the loop.
+            </p>
+          </div>
+          <div className="mt-8">
+            <p className="text-lg">
+              <strong>Let's sprint toward success together! 🏃💨</strong>
+            </p>
+            <p>
+              For inquiries and more information, contact us at [Insert Contact Information].
+            </p>
+          </div>
+      </div>
+    )
+  }
+  
+  export default StartupSprint2023
